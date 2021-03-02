bad9880435ef912e11f3985ef24550c97d6b638d
samsimsom <samsimsom@gmail.com>
2021-03-02 16:04:24 +0300

    forms

--- Learning_FLASK/the_flask_mega_tutorial/app/routes.py
+++ Learning_FLASK/the_flask_mega_tutorial/app/routes.py
@@ -1,6 +1,10 @@
 
-from flask import render_template
+from flask import (render_template, 
+					flash, 
+					redirect, 
+					url_for)
 from app import app		# app package (directory) import app variable
+from app.forms import LoginForm
 
 @app.route('/')			# decorator from flask - URL
 @app.route('/index')	# more URL
@@ -29,6 +33,23 @@ def index():
 							title=title, 
 							user=user,
 							posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+	title = 'Login'
+	form = LoginForm()
+
+	if form.validate_on_submit():
+		flash(f'Login requested for User: {form.username.data}, '
+			  f'Remember Me: {form.remember_me.data}')
+
+		return redirect('/index')
+
+	return render_template('login.html', 
+							title=title, 
+							form=form)
 
 
 @app.route('/about')
