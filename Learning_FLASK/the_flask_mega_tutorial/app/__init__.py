304f00daaba25feed0a5d6ae9b4e467a896a04b8
samsimsom <samsimsom@gmail.com>
2021-03-02 19:26:07 +0300

    database

--- Learning_FLASK/the_flask_mega_tutorial/app/__init__.py
+++ Learning_FLASK/the_flask_mega_tutorial/app/__init__.py
@@ -3,11 +3,13 @@ import os
 
 from flask import Flask
 from config import Config
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)	# app instance
 app.config.from_object(Config)
-
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # circular dependency problemi yaratmamasi icin altta import ediyorum
-from app import routes	# routes moduel import
+from app import routes, models	# routes moduel import
--- Learning_FLASK/the_flask_mega_tutorial/config.py
+++ Learning_FLASK/the_flask_mega_tutorial/config.py
@@ -1,8 +1,13 @@
 
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
 	DEBUG = os.environ.get("DEBUG")
 	FLASK_ENV = os.environ.get("FLASK_ENV")
 	FLASK_APP = os.environ.get("FLASK_APP")
+
 	SECRET_KEY = os.environ.get("SECRET_KEY")
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
