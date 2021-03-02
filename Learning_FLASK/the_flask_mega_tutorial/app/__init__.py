2f5d7e75ffdc07d4cfc42f096ceefa68edc60c34
samsimsom <samsimsom@gmail.com>
2021-03-02 22:08:17 +0300

    formatting

--- Learning_FLASK/the_flask_mega_tutorial/app/__init__.py
+++ Learning_FLASK/the_flask_mega_tutorial/app/__init__.py
@@ -1,16 +1,16 @@
 
-import os
+"""
+Module Docstring
+"""
 
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
-app = Flask(__name__)	# app instance
+app = Flask(__name__)  # app instance
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
-# circular dependency problemi yaratmamasi icin altta import ediyorum
-from app import routes, models	# routes moduel import
+from app import routes, models
