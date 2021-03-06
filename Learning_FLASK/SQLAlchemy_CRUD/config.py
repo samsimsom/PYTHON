550637947bd2c52765a225a38fa50ea1c22aa20d
samsimsom <samsimsom@gmai.com>
2021-03-06 16:25:53 +0300

    base

---
+++ Learning_FLASK/SQLAlchemy_CRUD/config.py
@@ -0,0 +1,11 @@
+
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = os.environ.get('DEBUG')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    FLASK_APP = os.environ.get('FLASK_APP')
