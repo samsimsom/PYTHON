f16071be3c4670cb4e82fd71027f460ddfe7a21b
samsimsom <samsimsom@gmail.com>
2021-03-08 18:51:26 +0300

    DDD

--- Learning_FLASK/domain_driven_design/app.py
+++ Learning_FLASK/domain_driven_design/app.py
@@ -5,19 +5,21 @@ from database import database
 from apps.app1.views import app1
 # from apps.app2.views import app2
 
+
 def create_app():
     app = Flask(__name__)
     # setup with the configuration provided
     app.config.from_object('config.DevelopmentConfig')
-    
+
     # setup all our dependencies
     database.init_app(app)
-    
+
     # register blueprint
     app.register_blueprint(app1)
     # app.register_blueprint(app2, url_prefix="/app2")
-    
+
     return app
+
 
 if __name__ == "__main__":
     create_app().run()
--- Learning_FLASK/domain_driven_design/config.py
+++ Learning_FLASK/domain_driven_design/config.py
@@ -1,23 +1,28 @@
 
 import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = '57e19ea558d4967a552d03deece34a70'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 class ProductionConfig(Config):
     DEBUG = False
+    FLASK_APP = os.environ.get("FLASK_APP")
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class DevelopmentConfig(Config):
-	DEBUG = os.environ.get("DEBUG")
-	FLASK_ENV = os.environ.get("FLASK_ENV")
-	FLASK_APP = os.environ.get("FLASK_APP")
-	SECRET_KEY = os.environ.get("SECRET_KEY")
-	SQLALCHEMY_DATABASE_URI="sqlite:///development_database.db"
+    DEBUG = os.environ.get("DEBUG")
+    FLASK_ENV = os.environ.get("FLASK_ENV")
+    FLASK_APP = os.environ.get("FLASK_APP")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///development_database.db"
