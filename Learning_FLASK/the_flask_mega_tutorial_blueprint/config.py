acdd1abc2e417bdebb8b7a20fde5705fbd63f8dd
samsimsom <samsimsom@gmail.com>
2021-03-08 16:39:44 +0300

    format

--- Learning_FLASK/the_flask_mega_tutorial_blueprint/config.py
+++ Learning_FLASK/the_flask_mega_tutorial_blueprint/config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
+
 class Config(object):
     # DEBUG = os.environ.get("DEBUG")
     FLASK_ENV = os.environ.get("FLASK_ENV")
