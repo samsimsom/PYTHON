86ad0b79b04a210151111f5a3722a14fbe851a07
samsimsom <samsimsom@gmail.com>
2021-03-23 22:09:46 +0300

    up

--- FlaskAPI/01-RestAPI/app.py
+++ FlaskAPI/01-RestAPI/app.py
@@ -1,24 +1,33 @@
 
 
 from flask import (Flask,
-                   render_template)
-from flask_restful import Resource, Api
+                   render_template,
+                   request,
+                   make_response,
+                   jsonify)
 
 app = Flask(__name__)
-api = Api(app)
-
-
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
-
-
-api.add_resource(Student, '/student/<string:name>')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     title = 'Index'
-
     return render_template('index.html',
                            title=title)
+
+
+@app.route("/guestbook")
+def guestbook():
+
+
+    return render_template("index.html")
+
+
+@app.route("/guestbook/create-entry", methods=["POST"])
+def create_entry():
+
+    req = request.get_json()
+    print(req)
+    res = make_response(jsonify(req), 200)
+
+    return res
