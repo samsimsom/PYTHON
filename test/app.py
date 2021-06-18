c2e664a886a72d70ab784113e22a18786eabd993
samsimsom <samsimsom@gmail.com>
2021-06-18 21:58:18 +0300

    test

---
+++ test/app.py
@@ -0,0 +1,12 @@
+
+
+from person import Person
+
+person1 = Person('ahmet', 34)
+person2 = Person('kamil', 44)
+
+print(person1.say_hello())
+print(person2.say_hello())
+
+print(person1.calculate_attack(32))
+print(person2.calculate_attack(22))
---
+++ test/person.py
@@ -0,0 +1,18 @@
+
+
+from random import randint
+
+
+class Person(object):
+    def __init__(self, name, age) -> None:
+        super().__init__()
+        self.name = name
+        self.age = age
+
+    def say_hello(self):
+        return f'Hello, my name is {self.name}'
+
+    def calculate_attack(self, damege):
+        random_number = randint(0, 30)
+        result = damege + random_number
+        return result
