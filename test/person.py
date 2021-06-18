9af91b96509a20887dbff4a4914d3d6fcd3aa5d4
samsimsom <samsimsom@gmail.com>
2021-06-18 22:10:53 +0300

    up

--- test/person.py
+++ test/person.py
@@ -9,10 +9,10 @@ class Person(object):
         self.name = name
         self.age = age
 
-    def say_hello(self):
+    def say_hello(self) -> str:
         return f'Hello, my name is {self.name}'
 
-    def calculate_attack(self, damege):
+    def calculate_attack(self, damege) -> int:
         random_number = randint(0, 30)
         result = damege + random_number
         return result
